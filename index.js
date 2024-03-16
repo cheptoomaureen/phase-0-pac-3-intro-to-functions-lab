15d702abec50f6e08c23b9135601a89590222f87
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-16 20:55:24 +0300

    functions assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function shout(string) {
+    return string.toUpperCase();
+  
+  }function whisper(string) {
+    return string.toLowerCase();
+  }
+    function logShout(string) {
+        console.log(string.toUpperCase());
+      }function logWhisper(string) {
+        console.log(string.toLowerCase());
+      }      
+      function sayHiToHeadphonedRoommate(string) {
+        if (string === string.toLowerCase()) {
+          return "I can't hear you!";
+        } else {
+          return ;      
+        }
+      } function sayHiToHeadphonedRoommate(string) {
+        if (string === string.toUpperCase()) {
+          return "YES INDEED!";
+        } else {
+          return "I can't hear you!";
+        }
+      }function sayHiToHeadphonedRoommate(string) {
+        if (string === string.toLowerCase()) {
+          return "I can't hear you!";
+        } else {
+          return "YES INDEED!";
+        }
+      }function sayHiToHeadphonedRoommate(string) {
+        if (string.toLowerCase() === "let's have dinner together!") {
+          return "I would love to!";
+        } else if (string === string.toUpperCase()) {
+          return "YES INDEED!";
+        } else {
+          return "I can't hear you!";
+        }
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+
+  
+  
